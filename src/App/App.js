12562ce22eb35b0bae7d939b89ea4f699e87556f
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-10-17 09:09:23 -0600

    Initial commit

--- src/App/App.js
+++ src/App/App.js
@@ -4,16 +4,12 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <div className='resy-form'>
-
-        </div>
-        <div className='resy-container'>
-          
-        </div>
+        <div className='resy-form'></div>
+        <div className='resy-container'></div>
       </div>
-    )
+    );
   }
 }
 
