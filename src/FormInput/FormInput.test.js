5d01a5ba66a08db56b6d23f3b432c83458fbf049
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-10-17 10:28:18 -0600

    Add state change test for FormInput when handling change with form submission

--- src/FormInput/FormInput.test.js
+++ src/FormInput/FormInput.test.js
@@ -13,4 +13,13 @@ describe('FORMINPUT', () => {
   it('should match FormInput Snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should update state when handleChange is called', () => {
+    const mockEvent = { target: { name: 'name', value: 'TravisRobbie' } };
+    const expected = 'TravisRobbie';
+
+    wrapper.instance().handleChange(mockEvent);
+
+    expect(wrapper.state('name')).toEqual(expected);
+  });
 });
