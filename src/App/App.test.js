4882a1f81954a8dfdb903647f4468339abd70c79
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-10-17 10:17:15 -0600

    Add first test to test for change in state

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -8,6 +8,7 @@ describe('APP', () => {
   beforeEach(() => {
     wrapper = shallow(<App />);
   });
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
@@ -16,5 +17,25 @@ describe('APP', () => {
 
   it('should match App Snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should update state with a reservation when addReservation is called', () => {
+    // Setup
+    const mockReservation = {
+      name: 'Joshua',
+      date: '6/5',
+      time: '12:00',
+      number: 10
+    };
+    const expected = [mockReservation];
+
+    // Expectation
+    expect(wrapper.state('reservations')).toEqual([]);
+
+    // Execution
+    wrapper.instance().addReservation(mockReservation);
+
+    // Expectaion
+    expect(wrapper.state('reservations')).toEqual(expected);
   });
 });
